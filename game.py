c709597205b920b3c18c5f66659e3db60c792172
23sevi <23sevi@stockholmscience.se>
2024-04-26 12:48:23 +0200

    Revert "1"
    This reverts commit 656c119a71e6b363c1912ab5b2906f7ae30d2f44.

--- game.py
+++ game.py
@@ -1,146 +1,177 @@
-import random
-
 grid_size = 9
+player = [4, 5]
+door = [8, 4]
+chest = [8, 0]
+lake = [7,1],[7,2],[7,0],[7,3],[7,4],[7,5],[7,6],[7,8],[7,9],[6,0],[6,1],[6,2],[6,3],[6,4],[6,5],[6,6],[6,8],[6,9]
+brige = [7,7],[6,7]
+stig = [2,6],[3,6],[4,7],[5,7],[1,6],[3,7],[8,7]
+import random
+import time
+
+movment = 1
+def roll_d20():
+    return random.randint(1, 20)
 
 
-class Place:
-    def __init__(self, name, description, player_start):
-        self.name = name
-        self.description = description
-        self.player_start = player_start
-        self.objects = []
-
-    def GetName(self):
-        return self.name
-    
-    def addObject(self, obj):
-        self.objects.append(obj)
-
-    def removeObject(self, obj):
-        self.objects.remove(obj)
-
-    def getObjects(self):
-        return self.objects
-
-class gameObject:
-    def __init__(self, x, y, name, emoji, place):
-        self.x = x
-        self.y = y
-        self.name = name
-        self.emoji = emoji
-        self.place = place
-
-        place.addObject(self)
-    
-    def getPosition(self):
-        return [self.x, self.y]
-
-    def setPosition(self, x, y):
-        self.x = x
-        self.y = y
-
-    def setPlace(self, place):
-        self.place = place
-        
-    def getPlace(self):
-        return self.place
-
-
-places = {
-    "house": Place("house", "You are inside the house.", [4, 5]),
-    "outside": Place("outside", "You are outside the house.", [1, 6])
-}
-
-class Link(gameObject):
-    def __init__(self, x, y, name, emoji, place,destination = Place):
-        super().__init__(x, y, name, emoji, place)
-        self.destination = destination
-
-    def interact(self):
-        global currentPlace 
-        currentPlace.removeObject(player)
-        currentPlace = self.destination
-        currentPlace.addObject(player)
-
-        x,y = places[currentPlace.GetName()].player_start
-        player.setPosition(x,y)
-        print_grid()
-        print(currentPlace.description)
-        
-
-links = [
-    Link(3, 4, "door", "🚪", places["house"],places["outside"]),
-    Link(0, 6, "house", "🏠", places["outside"],places["house"])
-]
+antal_rullingar = 1
 
 
 
 
-def move_player():
-    move = input("Where do you want to go? (w/s/a/d): ").lower()
-    if move == "q":
-        print("Exiting the game.")
-        return False
-
-    x, y = player.getPosition()
-    if move == "w" and x > 0:
-        x -= 1
-    elif move == "s" and x < grid_size - 1:
-        x += 1
-    elif move == "a" and y > 0:
-        y -= 1
-    elif move == "d" and y < grid_size - 1:
-        y += 1
-
-    #If go through door
-
-    for link in links:
-        if link.place == currentPlace:
-            if [x, y] == link.getPosition():
-                link.interact()
-                return True
-
-    player.setPosition(x, y)
-    return True
 
 def print_grid():
-    print(currentPlace)
     for i in range(grid_size):
         for j in range(grid_size):
-            #Loop all gameobjects
-            object_found = False
-            obj = gameObject
-            
-            for obj in currentPlace.objects:
-                if [i,j] == obj.getPosition():
-                    object_found = True
-                    break
-                else:
-                    object_found = False
-
-            if object_found:
-                print(obj.emoji, end=" ")
+            if [i, j] == player:
+                print("🈸", end=" ")
+            elif [i, j] == door:
+                print("🚪", end=" ")
+            elif [i, j] == chest:
+                print("💾", end=" ")
             else:
                 print("⬛", end=" ")
         print()
 
-enemy = gameObject(3, 3, "enemy", "🦧", places["outside"])
-chest = gameObject(8, 0, "chest", "💾", places["house"])
-player = gameObject(4, 5, "player", "✳️ ", places["house"])
-lake = gameObject([7,1],[7,2],[7,0],[7,3],[7,4],[7,5],[7,6],[7,8],[7,9],[6,0],[6,1],[6,2],[6,3],[6,4],[6,5],[6,6],[6,8],[6,9]"lake", "🟦", places["outside"])
-bridge = gameObject(7,7,6,7 "bridge", "🟫", )
-currentPlace = places["house"]
-player.setPlace(currentPlace)
+def move_player(move):
+    global player
+    x, y = player
+    if move == "w" and x > 0:
+        player = [x - 1, y]
+    elif move == "s" and x < grid_size - 1:
+        player = [x + 1, y]
+    elif move == "a" and y > 0:
+        player = [x, y - 1]
+    elif move == "d" and y < grid_size - 1:
+        player = [x, y + 1]
+def outside():
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if[i, j] == player:
+                print("🈸", end=" ")
+            elif [i ,j] == house:
+                print("🏠", end=" ")
+            elif [i ,j] in lake:
+                print("🟦", end=" ")
+            elif [i, j] == enemy:
+                print("🦧", end=" ")
+            elif [i, j] in brige:
+                print("🟫", end=" ")
+            elif [i, j] in stig:
+                print("⬛", end=" ")    
+            else:
+                print("🟩", end=" ")
+        print()
 
-def main():
-    print("Welcome to the game!")
-    print("Instructions: Move using 'w', 'a', 's', 'd'. Type 'q' to quit.")
+def youded():
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if [i, j] == player:
+                print("💀", end=" ")
+            else:
+                print("⬛", end=" ")
+        print()        
+                
+def respawn():
+    print()
+
+    
+
+
+
+class weapon:
+    def __init__(self, damage, Durability):
+        self.damage = damage
+        self.Durability = Durability
+        
+
+Sword = weapon(2,10)
+
+
+
+print_grid()
+
+enemy = []
+
+while True:
+    move = input("Vart vill du gå? (w/s/a/d): ").lower()
+    if move == "q":
+        print("Avslutar spelet.")
+        break
+    move_player(move)
     print_grid()
 
-    while True:
-        if not move_player():
-            break
-        print_grid()
+    if player == chest:
+        sword = input("You want to take the sword?(yes/no)").lower()
+        if sword == "yes":
+            print("You gained the sword")
+        elif sword == "no":
+            print("You don't like sharp things pussy")    
+    
+    if player == door:
+        answer = input("You want to go outside? (yes/no): ").lower()
+        if answer == "yes":
+            print("Go go outside")
+            player = [1, 6]
+            house = [0, 6]
+            enemy = [3, 3]
+            
+            
 
-if __name__ == "__main__":
-    main()
+            outside() 
+
+
+            while True:
+                    move = input("Vart vill du gå? (w/s/a/d): ").lower()
+                    if move == "q":
+                        print("Avslutar spelet.")
+                        break
+                    move_player(move)
+                    outside()
+
+                    enemy[1] += movment
+                    if enemy[1] == 0 or enemy[1] == grid_size - 1:
+                        movment *= -1
+
+                    if player == house:
+                        break
+                    if player in lake:
+                        print("You can't swim you idiot")
+                        time.sleep(2)
+                        youded()
+                        exit()
+
+                    if player == enemy:
+                        fight = input("Want to fight the monster yes or no: ")
+                        if fight == "yes":
+                            time.sleep(2)
+                            print("roll for damage")
+
+                            for i in range(antal_rullingar):
+                                
+                                resulat = roll_d20()
+                                print(f"Dice {i+1}: {resulat}")
+                                time.sleep(2)
+
+                                if(resulat > 10):
+                                    print("You have succesfully killed the monster")
+                                    for i in range(grid_size):
+                                        for j in range(grid_size):
+                                            if [i, j] == enemy:
+                                                print("💀", end=" ")
+                                    
+                                    
+                                else:
+                                    youded()
+                                    time.sleep(2)
+                                    print("you died")
+                                    exit()
+                                    
+                        elif fight == "no":
+                    
+                            print("nice")    
+
+
+
+            
+        else:
+            print("you stay inside")
